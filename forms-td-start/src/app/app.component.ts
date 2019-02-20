a6a1b7cdc1d86829918ef64bfb1b343296d65ebe
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-20 17:25:37 +0200

    form patchValue to change an input

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,6 +15,11 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      UserData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
