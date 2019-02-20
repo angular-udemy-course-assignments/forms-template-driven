1ef851c69a072818fd6c0f5d9491289f2b1eaa57
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-20 14:48:14 +0200

    forms - 2 way binding

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,8 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer: string;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
